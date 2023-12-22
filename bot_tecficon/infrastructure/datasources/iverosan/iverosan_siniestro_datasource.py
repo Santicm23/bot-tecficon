590fbff7f66c4f80d0c6fcf4cdd6853be3702570
Santicm23 <santicastrom@outlook.com>
2023-12-22 18:59:35 -0500

    guardando foto del logo y continuando bot

--- bot_tecficon/infrastructure/datasources/iverosan/iverosan_siniestro_datasource.py
+++ bot_tecficon/infrastructure/datasources/iverosan/iverosan_siniestro_datasource.py
@@ -25,6 +25,7 @@ class IverosanSiniestrosDatasource(SiniestrosDatasource):
     def get_siniestro_by_id(self, siniestro_id: int) -> Siniestro:
         self.driver = webdriver.Chrome()
 
+        # Login
         self.driver.get(environment.BASE_URL_ALLIA2)
         self.driver.set_window_size(968, 612)
         self.driver.find_element(By.ID, "nx-input-0").send_keys(environment.USER_ALLIA2)
@@ -32,6 +33,8 @@ class IverosanSiniestrosDatasource(SiniestrosDatasource):
         self.driver.find_element(By.ID, "nx-input-1").send_keys(environment.PASS_ALLIA2)
         self.driver.find_element(By.CSS_SELECTOR, ".nx-button__content-wrapper").click()
         time.sleep(5)
+
+        # Entrar a página de siniestros
         element = self.driver.find_element(By.CSS_SELECTOR, ".c-main-navbar__link")
         actions = ActionChains(self.driver)
         actions.move_to_element(element).click().perform()
@@ -41,16 +44,60 @@ class IverosanSiniestrosDatasource(SiniestrosDatasource):
         time.sleep(2)
         self.driver.switch_to.frame(0)
         self.driver.find_element(By.ID, "PENDING").click()
+
+        # Buscar siniestro
         try:
             self.driver.find_element(By.XPATH, f"//*[contains(text(), '{siniestro_id}')]").click()
         except:
+            # Si no existe el siniestro, cerrar el driver y lanzar excepción
             self.driver.quit()
             raise SiniestroNoExisteError(siniestro_id)
+
+        placa = self.driver.find_element(By.ID, 'vehiclePlate').text
+        fecha_asignacion_siniestro = self.driver.find_element(By.ID, 'orderDate').text
+        ciudad = self.driver.find_element(By.ID, '').text # TODO
+        dpto = self.driver.find_element(By.ID, 'incidentLocation_node1').text
+        producto = self.driver.find_element(By.ID, 'productName').text
+        fecha_accidente = self.driver.find_element(By.ID, 'incidentDate').text
+        direccion_ocurrencia = self.driver.find_element(By.ID, 'incidentAddress').text
+        tipo_encargo = self.driver.find_element(By.ID, 'orderTypeDesc').text
+        concepto_responsabilidad = self.driver.find_element(By.ID, '').text # TODO
+        lesiones = self.driver.find_element(By.ID, '').text # TODO
+        etapa_proceso_penal = self.driver.find_element(By.ID, '').text # TODO
+        tramitador = self.driver.find_element(By.ID, 'tramitName').text
+        mediador = self.driver.find_element(By.ID, 'agentName').text # FIXME
+        tomador = self.driver.find_element(By.ID, 'holderName').text
+        propietario = self.driver.find_element(By.ID, 'ownerName').text
+        conductor_asegurado = self.driver.find_element(By.ID, 'driverName').text # FIXME
+        informe_abogado = self.driver.find_element(By.ID, 'lawyer_tipoDoc').text # FIXME
 
         time.sleep(5)
         self.driver.quit()
 
-        raise
+        siniestro = Siniestro(
+            numero_siniestro=str(siniestro_id),
+            placa=placa,
+            fecha_asignacion_siniestro=fecha_asignacion_siniestro,
+            ciudad=ciudad,
+            dpto=dpto,
+            producto=producto,
+            fecha_accidente=fecha_accidente,
+            direccion_ocurrencia=direccion_ocurrencia,
+            tipo_encargo=tipo_encargo,
+            concepto_responsabilidad=concepto_responsabilidad,
+            lesiones=lesiones,
+            etapa_proceso_penal=etapa_proceso_penal,
+            tramitador=tramitador,
+            mediador=mediador,
+            tomador=tomador,
+            propietario=propietario,
+            conductor_asegurado=conductor_asegurado,
+            informe_abogado=informe_abogado
+        )
+
+        print(siniestro)
+
+        return siniestro
 
     @override
     def add_siniestro(self, siniestro: Siniestro) -> bool:

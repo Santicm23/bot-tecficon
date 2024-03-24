33b1f65776827ed45969bc51cd96bdef717ed236
Santicm23 <santicastrom@outlook.com>
2024-03-23 19:17:39 -0500

    creando entidad victimas

--- bot_tecficon/domain/entities/victima.py
+++ bot_tecficon/domain/entities/victima.py
@@ -1,7 +1,35 @@
-
 from dataclasses import dataclass
+
+"""
+{
+    "s.numero_siniestro": "107431314",
+    "v.id_victima_sin": "2",
+    "v.id_siniestro": "2",
+    "v.nombre_victima": "Jader francisco medina",
+    "v.telefono_victima": "3052344000",
+    "v.apoderado_victima": "",
+    "v.id_delito": "2",
+    "v.id_despacho": "489",
+    "v.id_juzgado": "",
+    "v.id_estado_victima": "1",
+    "v.id_etapa_proceso_civil": "",
+    "v.id_pago": "2",
+    "v.obs_abogado": "",
+    "v.ult_valor_autorizado": "",
+    "v.ult_valor_ofrecido_HYG": "",
+    "v.valor_conciliado": "",
+    "v.valor_estimado_conc": "",
+    "v.valor_perdida_liquidada_por_HYG": "",
+    "v.valor_pretension": "",
+    "sincronizarfu": "N"
+}
+"""
 
 
 @dataclass
 class Victima:
-    ...
+    id_siniestro: int
+    nombre: str
+    telefono: str
+    apoderado: str
+    observacion_abogado: str
--- bot_tecficon/infrastructure/datasources/sinapp/sinapp_victimas_datasource.py
+++ bot_tecficon/infrastructure/datasources/sinapp/sinapp_victimas_datasource.py
@@ -1,14 +1,29 @@
+import json
 from typing import override
 
+import requests
+
+from ....config.constants import environment
 from ....domain.datasources import VictimasDatasource
 from ....domain.entities import Victima
 
 
 class SinappVictimasDatasource(VictimasDatasource):
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.params = {"tok": environment.TOK}
+
     @override
-    def get_victimas_by_siniestro(self, id_siniestro: str) -> list[Victima]:
+    def get_victimas_by_siniestro(self, id_siniestro: int) -> list[Victima]:
+        res = self.session.get(
+            f"{environment.BASE_URL_SINAPP}/ep_ver_v_siniestros/", timeout=5
+        )
+
+        data = res.text
+        data = json.loads(data, strict=False)
+
         raise
 
     @override
-    def add_victima_to_siniestro(self, victima: Victima, id_siniestro: str) -> None:
+    def add_victima_to_siniestro(self, victima: Victima, id_siniestro: int) -> None:
         raise
